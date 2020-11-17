6fbff11513a4354d75d2857451d334d31ef0b466
Johan Rivera <don_noruego@outlook.com>
2020-11-17 14:31:45 -0500

    Agregados los archivos iniciales

---
+++ index.php
@@ -0,0 +1,205 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<?php require('header.php'); ?>
+	<!-- start slider -->
+       <div id="fwslider">
+         <div class="slider_container">
+            <div class="slide"> 
+                <!-- Slide image -->
+               <img src="images/slider1.jpg" class="img-responsive" alt=""/>
+                <!-- /Slide image -->
+                <!-- Texts container -->
+                <div class="slide_content">
+                    <div class="slide_content_wrap">
+                        <!-- Text title -->
+                        <h1 class="title">Comunidad <br>CPADT</h1>
+                        <!-- /Text title -->
+                        <div class="button"><a href="#">Conocer más</a></div>
+                    </div>
+                </div>
+               <!-- /Texts container -->
+            </div>
+            <!-- /Duplicate to create more slides -->
+            <div class="slide">
+               <img src="images/slider2.jpg" class="img-responsive" alt=""/>
+                <div class="slide_content">
+                    <div class="slide_content_wrap">
+					<h1 class="title">Comunidad <br>CPADT</h1>
+                        <!-- /Text title -->
+                        <div class="button"><a href="#">Conocer más</a></div>
+                    </div>
+                </div>
+            </div>
+            <!--/slide -->
+        </div>
+        <div class="timers"></div>
+        <div class="slidePrev"><span></span></div>
+        <div class="slideNext"><span></span></div>
+       </div>
+       <!--/slider -->
+      </div>
+	  <div class="main">
+		<div class="content-top">
+			<h2>Servicios</h2>
+			<p>Gran espacio, servicios de primera calidad por el mejor precio.</p>
+			<div class="close_but"><i class="close1"> </i></div>
+				<ul id="flexiselDemo3">
+				<li><img src="images/board1.jpg" /></li>
+				<li><img src="images/board2.jpg" /></li>
+				<li><img src="images/board3.jpg" /></li>
+				<li><img src="images/board4.jpg" /></li>
+				<li><img src="images/board5.jpg" /></li>
+			</ul>
+		<h3></h3>
+			
+		</div>
+	</div>
+	<div class="content-bottom">
+		<div class="container">
+			<div class="row content_bottom-text">
+			  <div class="col-md-7">
+				<h3>La Corporación<br>CPADT</h3>
+				<p class="m_1">Proin posuere ornare metus, id tincidunt ipsum fermentum vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam placerat aliquet metus sit amet posuere. Praesent a quam ut ante dictum fermentum in et mauris. Sed fermentum nunc ac mi tempor interdum. Nulla aliquam laoreet sem id posuere. Quisque eget metus diam. Etiam magna est, vulputate vitae rutrum sit amet, congue nec felis. Nunc et vehicula est. Etiam iaculis, risus at bibendum tristique, ex tellus varius tellus, sit amet euismod nibh risus non felis. Nam turpis mi, suscipit sed pharetra sit amet, hendrerit eu ligula. Aliquam erat volutpat. In hac habitasse platea dictumst. Integer vehicula finibus porta. Quisque hendrerit eros at urna mollis bibendum quis nec ipsum. </p>
+				<p class="m_2">Mauris a arcu quis eros venenatis ultrices sit amet sit amet ex. Donec dui elit, luctus fringilla sem ut, pulvinar sagittis nisl. Fusce viverra, dui eget varius efficitur, magna elit pharetra lorem, quis iaculis neque nunc ac sapien. Fusce imperdiet convallis luctus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed at nisi id ex vehicula bibendum. Nulla pellentesque sem blandit dapibus malesuada. Nam blandit, elit sed aliquet semper, nulla nibh laoreet turpis, volutpat varius est nibh eu libero. Fusce id dui ut diam maximus molestie. Praesent feugiat tincidunt ante et varius. Vivamus vestibulum ex interdum orci accumsan tempor. Nam convallis, eros vel semper dapibus, nibh metus consequat nunc, eu suscipit lectus sem sit amet arcu</p>
+			  </div>
+			</div>
+		</div>
+	</div>
+	<div class="features">
+		<div class="container">
+			<h3 class="m_3">Novedades</h3>
+			<div class="close_but"><i class="close1"> </i></div>
+			  <div class="row">
+				<div class="col-md-3 top_box">
+				  <div class="view view-ninth"><a href="single.php">
+                    <img src="images/pic1.jpg" class="img-responsive" alt=""/>
+                    <div class="mask mask-1"> </div>
+                    <div class="mask mask-2"> </div>
+                      <div class="content">
+                        <h2>Hover Style #9</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing.</p>
+                      </div>
+                   </a> </div
+                  </div>
+                  <h4 class="m_4"><a href="#">nostrud exerci ullamcorper</a></h4>
+                  <p class="m_5">claritatem insitam</p>
+                </div>
+                <div class="col-md-3 top_box">
+					<div class="view view-ninth"><a href="single.php">
+                    <img src="images/pic2.jpg" class="img-responsive" alt=""/>
+                    <div class="mask mask-1"> </div>
+                    <div class="mask mask-2"> </div>
+                      <div class="content">
+                        <h2>Hover Style #9</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing.</p>
+                      </div>
+                    </a> </div>
+                   <h4 class="m_4"><a href="#">nostrud exerci ullamcorper</a></h4>
+                   <p class="m_5">claritatem insitam</p>
+				</div>
+				<div class="col-md-3 top_box">
+					<div class="view view-ninth"><a href="single.php">
+                    <img src="images/pic3.jpg" class="img-responsive" alt=""/>
+                    <div class="mask mask-1"> </div>
+                    <div class="mask mask-2"> </div>
+                      <div class="content">
+                        <h2>Hover Style #9</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing.</p>
+                      </div>
+                    </a> </div>
+                   <h4 class="m_4"><a href="#">nostrud exerci ullamcorper</a></h4>
+                   <p class="m_5">claritatem insitam</p>
+				</div>
+				<div class="col-md-3 top_box1">
+					<div class="view view-ninth"><a href="single.php">
+                    <img src="images/pic4.jpg" class="img-responsive" alt=""/>
+                    <div class="mask mask-1"> </div>
+                    <div class="mask mask-2"> </div>
+                      <div class="content">
+                        <h2>Hover Style #9</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing.</p>
+                      </div>
+                     </a> </div>
+                   <h4 class="m_4"><a href="#">nostrud exerci ullamcorper</a></h4>
+                   <p class="m_5">claritatem insitam</p>
+				</div>
+			</div>
+		 </div>
+	    </div>
+		<div class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						<ul class="footer_box">
+							<h4>Products</h4>
+							<li><a href="#">Mens</a></li>
+							<li><a href="#">Womens</a></li>
+							<li><a href="#">Youth</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<ul class="footer_box">
+							<h4>About</h4>
+							<li><a href="#">Careers and internships</a></li>
+							<li><a href="#">Sponserships</a></li>
+							<li><a href="#">team</a></li>
+							<li><a href="#">Catalog Request/Download</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<ul class="footer_box">
+							<h4>Customer Support</h4>
+							<li><a href="#">Contact Us</a></li>
+							<li><a href="#">Shipping and Order Tracking</a></li>
+							<li><a href="#">Easy Returns</a></li>
+							<li><a href="#">Warranty</a></li>
+							<li><a href="#">Replacement Binding Parts</a></li>
+						</ul>
+					</div>
+					<div class="col-md-3">
+						<ul class="footer_box">
+							<h4>Newsletter</h4>
+							<div class="footer_search">
+				    		   <form>
+				    			<input type="text" value="Enter your email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter your email';}">
+				    			<input type="submit" value="Go">
+				    		   </form>
+					        </div>
+							<ul class="social">	
+							  <li class="facebook"><a href="#"><span> </span></a></li>
+							  <li class="twitter"><a href="#"><span> </span></a></li>
+							  <li class="instagram"><a href="#"><span> </span></a></li>	
+							  <li class="pinterest"><a href="#"><span> </span></a></li>	
+							  <li class="youtube"><a href="#"><span> </span></a></li>										  				
+						    </ul>
+		   					
+						</ul>
+					</div>
+				</div>
+				<div class="row footer_bottom">
+				    <div class="copy">
+			           <p>© 2014 Template by <a href="http://w3layouts.com" target="_blank">w3layouts</a></p>
+		            </div>
+					  <dl id="sample" class="dropdown">
+				        <dt><a href="#"><span>Change Region</span></a></dt>
+				        <dd>
+				            <ul>
+				                <li><a href="#">Australia<img class="flag" src="images/as.png" alt="" /><span class="value">AS</span></a></li>
+				                <li><a href="#">Sri Lanka<img class="flag" src="images/srl.png" alt="" /><span class="value">SL</span></a></li>
+				                <li><a href="#">Newziland<img class="flag" src="images/nz.png" alt="" /><span class="value">NZ</span></a></li>
+				                <li><a href="#">Pakistan<img class="flag" src="images/pk.png" alt="" /><span class="value">Pk</span></a></li>
+				                <li><a href="#">United Kingdom<img class="flag" src="images/uk.png" alt="" /><span class="value">UK</span></a></li>
+				                <li><a href="#">United States<img class="flag" src="images/us.png" alt="" /><span class="value">US</span></a></li>
+				            </ul>
+				         </dd>
+	   				  </dl>
+   				</div>
+			</div>
+		</div>
+<?php require('footer.php'); ?>
